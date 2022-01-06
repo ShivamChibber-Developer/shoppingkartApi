9bcbd85cc8e61e4951f8a2aed20b0dc61912cc84
ShivamChibber-Developer <shivjesus@gmail.com>
2022-01-06 23:17:18 +0530

    added new

--- app.js
+++ app.js
@@ -108,7 +108,7 @@ app.get('/details/:id', (req,res) => {
 // FIND ITEM 
 app.post('/menuItem', (req,res) => {
     console.log(req.body)
-    db.collection("shopping").find({id:{$in:req.body.ids}}).toArray((err, result) => {
+    db.collection("shopping").find({id:{$in:req.body}}).toArray((err, result) => {
         if (err) throw err;
         res.send(result);
     });
